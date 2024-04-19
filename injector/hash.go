4665df28cd329c0fc2d9beb3aa548547253972dd
alex <ovechko.056@gmail.com>
2024-04-19 17:44:39 +0200

    Crc32 hash (zlib)

--- injector/hash.go
+++ injector/hash.go
@@ -3,7 +3,9 @@ package injector
 import (
 	"io"
 	"crypto/sha1"
+	"hash/crc32"
 	"encoding/binary"
+
 	"chrome-poc/cutil"
 )
 
@@ -17,4 +19,8 @@ func entryHash(cacheKey string) uint64 {
  
 func persistentHash(data []byte) uint32 {
 	return cutil.SuperFastHash(data)
+}
+
+func crc32hash(data []byte) uint32 {
+	return crc32.ChecksumIEEE(data)
 }
